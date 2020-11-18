3d13da15ee0863e7d464b1d94decd79376c7a489
EloGko <74670100+EloGko@users.noreply.github.com>
2020-11-18 17:49:13 +0100

    Update ExoGit.py
    ça avance trkl

--- ExoGit.py
+++ ExoGit.py
@@ -7,3 +7,13 @@ def afficheGrille(grille):
     for i in range(0,3):
         print(grille[3*i],grille[3*i+1],grille[3*i+2],"\n")
     return
+
+def ajouteSymbole(grille, joueur):
+    choixIncorrect = "vrai"
+    while choixIncorrect = "vrai" :
+        i = input("Sur quelle ligne voulez-vous jouez ?")
+        j = input("Sur quelle colonne voulez-vous jouez ?")
+        if grille[3*i+j] != " ":
+            grille[3*i+j] = joueur
+            choixIncorrect = "faux"
+    return
