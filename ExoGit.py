bb3475dae5cf774c2c80df591dba62c66f7510b8
EloGko <74670100+EloGko@users.noreply.github.com>
2020-11-25 17:38:13 +0100

    on avance

--- ExoGit.py
+++ ExoGit.py
@@ -1,3 +1,4 @@
+#FONCTIONS :
 def initialiseGrille(grille):
     for i in range(0,9):
         grille[i] = " "
@@ -21,6 +22,30 @@ def ajouteSymbole(grille, joueur):
 def testeVictoireVerticale(grille):
     compteur = 0
     while compteur < 3:
-        if grille[compteur] != " " and grille[compteur] == grille[compteur+3] and grille[compteur+6]:
+        if grille[compteur] != " " and grille[compteur] == grille[compteur+3] and grille[compteur] == grille[compteur+6]:
             return "vrai"
     return "faux"
+
+def testeVictoireHorizontale(grille):
+    compteur = 0
+    while compteur < 3:
+        if grille[compteur] != " " and grille[compteur*3] == grille[compteur*3+1] and grille[compteur*3] == grille[compteur*3+2]:
+            return "vrai"
+    return "faux"
+    
+def testeVictoireDiagonale(grille):
+    if grille[compteur] != " " and grille[4] == grille[0] and grille[4] == grille [8]:
+        return "vrai"
+    if grille[compteur] != " " and grille[4] == grille[2] and grille[4] == grille [6]:
+        return "vrai"
+    return "faux"
+
+def testeJeuNul(gagnantTrouvé, tour):
+    if tour == 9 and gagnantTrouvé = "faux":
+        return "vrai"
+    return "faux"
+
+#CODE PRINCIPAL :
+initialiseGrille(grille)
+afficheGrille(grille)
+ajouteSymbole(grille, joueur)
